c56017587a8431159eec14d196ac76f9175f7a6b
yhklsdf666 <1991987275@qq.com>
2018-05-21 17:01:06 +0800

    2. 两数相加

--- LeetCode/2.两数相加.c
+++ LeetCode/2.两数相加.c
@@ -46,3 +46,22 @@ struct ListNode* addTwoNumbers(struct ListNode* l1, struct ListNode* l2) {
     
     return newhead;
 }
+
+/*官方答案*/
+/*
+class Solution {
+public:
+ListNode *addTwoNumbers(ListNode *l1, ListNode *l2) {
+    ListNode preHead(0), *p = &preHead;
+    int carry = 0;
+    while (l1 || l2 || carry) {
+        int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
+        carry = sum / 10;
+        p->next = new ListNode(sum % 10);
+        p = p->next;
+        l1 = l1 ? l1->next : l1;
+        l2 = l2 ? l2->next : l2;
+    }
+    return preHead.next;
+};
+*/
